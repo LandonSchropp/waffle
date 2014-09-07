396bc9ec7b97d03813268e81cecf40abfe33d389
Landon Schropp <schroppl@gmail.com>
2014-09-07 14:11:03 -0700

    Added a task to minify gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var rename = require("gulp-rename");
 
 gulp.task('clean', function() {
-  return gulp.src('build', {read: false})
+  return gulp.src('build/**/*', {read: false})
     .pipe(clean());
 });
 
-gulp.task('build', [ 'clean' ], function() {
+gulp.task('build', function() {
   return gulp.src('source/griddle.sass')
     .pipe(sass({style: 'expanded', lineNumbers: true}))
     .pipe(autoprefixer('last 2 versions', '> 1%', 'ie >= 10'))
@@ -16,4 +18,12 @@ gulp.task('build', [ 'clean' ], function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', [ "build" ]);
+gulp.task('minify', function() {
+  return gulp.src('build/griddle.css')
+    .pipe(minifyCSS())
+    .pipe(rename("griddle.min.css"))
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', [ 'clean', 'build', 'minify' ]);
