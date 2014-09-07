3fb90bc3756b89be361bf3674e88e71cad5f624b
Landon Schropp <schroppl@gmail.com>
2014-09-07 13:35:41 -0700

    Added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('clean', function() {
   return gulp.src('build', {read: false})
@@ -10,6 +11,7 @@ gulp.task('clean', function() {
 gulp.task('build', [ 'clean' ], function() {
   return gulp.src('source/griddle.sass')
     .pipe(sass({style: 'expanded', lineNumbers: true}))
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie >= 10'))
     .on('error', function (err) { console.log(err.message); })
     .pipe(gulp.dest('build'));
 });
